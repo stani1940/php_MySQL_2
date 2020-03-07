5ed3bbe3e08fcf6c38b7b37ead21551694052810
stani1940 <stanislav1940@abv.bg>
2020-03-07 10:32:52 +0200

    Add update.php file and fix create.php

--- crud_database_recipies/includes/db_connect.php
+++ crud_database_recipies/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
-$conn = mysqli_connect(localhost,root,spider81,recipies);
+$conn = mysqli_connect('localhost','root',"",'recipies');
 if (!$conn){
-    die("conection failed".mysqli_connect_error()." - ".mysqli_connect_errno());
+    die("Connection failed ".mysqli_connect_error()." - ".mysqli_connect_errno());
 }//else{
     //echo "Database connected successfully";
 //}
--- crud_database_recipies/index.php
+++ crud_database_recipies/index.php
@@ -4,8 +4,34 @@ include 'includes/header.php';
 ?>
 <h1>CRUD Database Units</h1>
 <div>
-    <button class="btn btn-success"><a href="create.php">Добави мерна единица</a> </button>
+    <button class="btn btn-success"><a href="create.php">Добави мерна единица</a></button>
 </div>
+
 <?php
+$read_query = "SELECT * FROM `units` WHERE `date_deleted` IS NULL";
+$result = mysqli_query($conn, $read_query);
+if (mysqli_num_rows($result) > 0) {
+    ?>
+    <table style="margin-left: 50px" class="table table-stripped">
+        <tr>
+            <td>id</td>
+            <td>unit_name</td>
+        </tr>
+        <?php
+        $num = 1;
+        while ($row = mysqli_fetch_assoc($result)){
+        ?>
+        <tr>
+            <td><?= $num++ ?></td>
+            <td><?= $row['unit_name'] ?></td>
+        <tr>
+            <?php
+            }
+            ?>
+    </table>
+    <?php
+}else{
+    die('Query failed'.mysqli_error($conn));
+}
 include 'includes/footer.php';
 ?>
